3fd10bcee970dc3a92a029da0333a46221da85cd
Jorge Meza Bello <jorberiver@gmail.com>
2023-05-01 01:40:37 -0600

    incluir header como archivo php

--- contacto.php
+++ contacto.php
@@ -1,28 +1,4 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Bienes Raíces</title>
-    <link rel="stylesheet" href="build/css/app.css">
-</head>
-<body>
-    <header class="header">
-        <div class="contenedor contenido-header">
-            <div class="barra">
-                <a href="/">
-                    <img src="build/img/logo.svg" alt="">
-                </a>
-                <nav class="navegacion">
-                    <a href="nosotros.html">Nosotros</a>
-                    <a href="anuncios.html">Anuncios</a>
-                    <a href="blog.html">Blog</a>
-                    <a href="contacto.html">Contacto</a>
-                </nav>
-            </div>
-        </div>
-    </header>
+<?php include "includes/templates/header.php";?>
     <main class="contenedor seccion contenido-centrado">
         <h1>Contacto</h1>
         <picture>
