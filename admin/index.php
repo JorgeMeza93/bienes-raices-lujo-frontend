e9e373b5caede2e0c2a4b852621dc88e111180f8
Jorge Meza Bello <jorberiver@gmail.com>
2023-05-14 22:44:55 -0600

    tabla listado de propiedades estilos

--- admin/index.php
+++ admin/index.php
@@ -10,6 +10,29 @@
         <?php endif?>
         <a href="/admin/propiedades/crear.php" class="boton boton-verde">Nueva Propiedad</a>
         
+        <table class="propiedades">
+            <thead>
+                <tr>
+                    <th>Id</th>
+                    <th>Título</th>
+                    <th>Imagen</th>
+                    <th>Precio</th>
+                    <th>Acciones</th>
+                </tr>
+            </thead>
+            <tbody>
+                <tr>
+                    <td>1</td>
+                    <td>Casa en la Playa</td>
+                    <td><img src="/imagenes/308be145d2f183af09859908ef91359e.jpg" class="imagen-tabla"></td>
+                    <td>$120000</td>
+                    <td>
+                        <a href="#" class="boton-rojo-block">Eliminar</a>
+                        <a href="#" class="boton-amarillo-block" >Actualizar</a>
+                    </td>
+                </tr>
+            </tbody>
+        </table>
     </main>
 <?php 
     includeTemplate("footer")
